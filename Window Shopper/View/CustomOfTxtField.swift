70bec84440219d8969b8bfa02e8cb2a3a1212750
AsmaaTarek <asmaatarek93@gmail.com>
2019-06-03 02:09:52 +0200

    added currecncy label

--- Window Shopper/View/CustomOfTxtField.swift
+++ Window Shopper/View/CustomOfTxtField.swift
@@ -9,9 +9,28 @@
 import UIKit
 @IBDesignable
 class CustomOfTxtField: UITextField {
+    override func draw(_ rect: CGRect) {
+   // draw currency label in text field
+        let size: CGFloat = 20
+        let currencyLabel = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        currencyLabel.backgroundColor = #colorLiteral(red: 0.4896606761, green: 0.5301663144, blue: 0.5890109454, alpha: 0.81)
+        currencyLabel.textAlignment = .center
+        currencyLabel.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLabel.layer.cornerRadius = 5.0
+        currencyLabel.clipsToBounds = true
+        //get currency symbol
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLabel.text = formatter.currencySymbol
+        addSubview(currencyLabel)
+        
+    }
+    
     override func prepareForInterfaceBuilder() {
         customizeView()
     }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
     customizeView()
@@ -20,7 +39,7 @@ class CustomOfTxtField: UITextField {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5.0
         textAlignment = .center
-        
+        clipsToBounds = true
         //change placeholder color
         if let placeHolder = placeholder{
             let place = NSAttributedString(string: placeHolder, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
